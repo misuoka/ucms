2b7e840ed2ac1dfbdd859c6b588ed6328d9b0081
misuoka <2214787353@qq.com>
2017-10-11 18:59:57 +0800

    修改入口加载

--- public/index.php
+++ public/index.php
@@ -10,7 +10,19 @@
 
 // [ 应用入口文件 ]
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-// // define('BIND_MODULE', 'index');
-// // 加载框架引导文件
-require __DIR__ . '/../vendor/topthink/framework/start.php';
+// define('APP_PATH', __DIR__ . '/../application/');
+// // // define('BIND_MODULE', 'index');
+// // // 加载框架引导文件
+// require __DIR__ . '/../vendor/topthink/framework/start.php';
+
+// [ 应用入口文件 ]
+namespace think;
+
+// ThinkPHP 引导文件
+// 加载基础文件
+require __DIR__ . '/../vendor/topthink/framework/base.php';
+
+// 支持事先使用静态方法设置Request对象和Config对象
+
+// 执行应用并响应
+Container::get('app')->run()->send();
